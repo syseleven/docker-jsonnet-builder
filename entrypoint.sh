86fc7b53385debfe5d1f1de7482af94ed513de7a
Bastian Hofmann <bashofmann@googlemail.com>
2018-11-30 19:21:43 +0100

    Only run jb install if the jsonnetfile lock file did not change (#1)
    * Only run jb install if the jsonnetfile lock file did not change
    * Quote variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,15 @@ set -e
 cd ${BASEDIR}
 
 if [ -f "jsonnetfile.lock.json" ] || [ -f "jsonnetfile.json" ]; then
-  jb install &>/dev/null
+  installed_hash=""
+  if [ -f "vendor/.installed.hash" ]	; then
+    installed_hash="$(cat vendor/.installed.hash)"
+  fi
+  current_hash="$(shasum -a 256 jsonnetfile.lock.json | cut -d " " -f 1)"
+  if [[ "${current_hash}" != "${installed_hash}" ]]; then
+    jb install &>/dev/null
+    echo -n "${current_hash}" > vendor/.installed.hash
+  fi
 fi
 
 exec jsonnet -J vendor "$@"
