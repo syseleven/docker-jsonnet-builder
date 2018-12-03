b3b1ec220c843a3b81ad053e54bd99cd5e33c350
Bastian Hofmann <bashofmann@gmail.com>
2018-12-03 13:17:12 +0100

    Fix shasum usage for alpine in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ if [ -f "jsonnetfile.lock.json" ] || [ -f "jsonnetfile.json" ]; then
   if [ -f "vendor/.installed.hash" ]	; then
     installed_hash="$(cat vendor/.installed.hash)"
   fi
-  current_hash="$(shasum -a 256 jsonnetfile.lock.json | cut -d " " -f 1)"
+  current_hash="$(sha256sum jsonnetfile.lock.json | cut -d " " -f 1)"
   if [[ "${current_hash}" != "${installed_hash}" ]]; then
     jb install &>/dev/null
     echo -n "${current_hash}" > vendor/.installed.hash
